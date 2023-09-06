e9eb3616909e46ecda852d553405033befc04f5c
jafarmh <jafar.m@yahoo.com>
2023-09-06 15:18:25 +0330

    required ohone number

--- app/Http/Requests/CustomerRequest.php
+++ app/Http/Requests/CustomerRequest.php
@@ -28,7 +28,7 @@ class CustomerRequest extends FormRequest
         $regexBankCardNumber = '/^(^[5|6]\d{3})-(\d{4})-(\d{4})-(\d{4})$/';
 
         return [
-            "PhoneNumber"=>'phone:IR',
+            "PhoneNumber"=>'required|phone:IR',
             "Firstname"=>"required|string|min:3",
             "Lastname"=>"required|string|min:3",
             "email"=>"required|email|unique:customers,email",
